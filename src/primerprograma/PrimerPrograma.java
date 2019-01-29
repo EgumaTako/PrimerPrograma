8dfda5614198c600dd5c749d1f2dc24bd10dfbb0
Emiliano <emigromtz_1@hotmail.com>
2019-01-29 08:01:49 -0600

    Kiubo raza

---
+++ src/primerprograma/PrimerPrograma.java
@@ -0,0 +1,19 @@
+package primerprograma;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PrimerPrograma {
+
+    public static BufferedReader entrada = new BufferedReader (new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws IOException {
+        int a;
+        System.out.println("Indique un valor numerico");
+        a = Integer.parseInt(entrada.readLine());
+        System.out.println("Valor de a = " + a);
+        System.out.println("----------------");
+    }
+    
+}
